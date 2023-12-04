e50dad595e6cd191ce8bed29386c0b73d0fa7cb0
Application <mojavenight@qq.com>
2023-12-04 16:48:55 +0800

    Bug Fixes
    bug fixes

--- SduNetCheckTool.Core/Tests/SystemProxyTest.cs
+++ SduNetCheckTool.Core/Tests/SystemProxyTest.cs
@@ -11,6 +11,8 @@ namespace SduNetCheckTool.Core.Tests
         {
             var data = new List<string>();
             var result = TestResult.Failed;
+            var result1 = TestResult.Failed;
+            var result2 = TestResult.Failed;
             try
             {
                 var proxyEnabled = RegUtil.RegReadValue(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings", "ProxyEnable", "-1");
@@ -22,7 +24,7 @@ namespace SduNetCheckTool.Core.Tests
                         break;
                     case "0":
                         proxyEnabledString = "关闭";
-                        result = TestResult.Success;
+                        result1 = TestResult.Success;
                         break;
                     case "-1":
                         proxyEnabledString = "获取失败";
@@ -35,13 +37,17 @@ namespace SduNetCheckTool.Core.Tests
                 data.Add($"系统全局代理状态:{proxyEnabledString}");
 
                 var PACproxyEnabledString = (RegUtil.IsExisted(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings", "AutoConfigURL") ? "开启" : "关闭");
+                if (PACproxyEnabledString == "关闭") result2 = TestResult.Success;
+
                 data.Add($"PAC代理状态:{PACproxyEnabledString}");
+
+                result = (result1 == TestResult.Success) && (result2 == TestResult.Success) ? TestResult.Success : TestResult.Failed;
             }
             catch (Exception)
             {
                 //ignored
             }
-            return new Tuple<TestResult, string, IRepair>(result,string.Join("\n",data), new ProxyRepair());
+            return new Tuple<TestResult, string, IRepair>(result, string.Join("\n", data), new ProxyRepair());
         }
     }
 }
