1fdd240ab6d8447fb3cbeef5df731615f9e25a35
Forgot-Dream <56014859+Forgot-Dream@users.noreply.github.com>
2024-03-11 19:56:52 +0800

    feat: 增加IPv6认证检测

--- SduNetCheckTool.Core/Tests/SduNetTest.cs
+++ SduNetCheckTool.Core/Tests/SduNetTest.cs
@@ -1,40 +1,61 @@
-﻿using SduNetCheckTool.Core.Repairs;
+﻿using System;
+using System.Text;
+using SduNetCheckTool.Core.Repairs;
 using SduNetCheckTool.Core.Utils;
-using System;
-using System.Collections.Generic;
 
-namespace SduNetCheckTool.Core.Tests
+namespace SduNetCheckTool.Core.Tests;
+
+public class SduNetTest : ITest
 {
-    public class SduNetTest : ITest
+    private bool _isSuccess;
+
+    public Tuple<TestResult, string, IRepair> Test()
     {
-        public Tuple<TestResult, string, IRepair> Test()
+        var strBuilder = new StringBuilder();
+        _isSuccess = true;
+
+        strBuilder.AppendLine("IPv4 认证：");
+        strBuilder.Append(GetSduNetAuthInfo("http://101.76.193.1/cgi-bin/rad_user_info"));
+
+        strBuilder.AppendLine();
+
+        strBuilder.AppendLine("IPv6 认证：");
+        strBuilder.Append(GetSduNetAuthInfo("http://[2001:250:5800:11::1]/cgi-bin/rad_user_info"));
+
+        return new Tuple<TestResult, string, IRepair>(
+            _isSuccess ? TestResult.Success : TestResult.Failed,
+            strBuilder.ToString(),
+            null
+        );
+    }
+
+    private string GetSduNetAuthInfo(string url)
+    {
+        try
         {
-            var data = new List<string>();
-            
-            string text;
-            try
-            {
-                var response = HttpUtil.GetHttpResponse("http://101.76.193.1/cgi-bin/rad_user_info");
-                response.EnsureSuccessStatusCode();
-                text = response.Content.ReadAsStringAsync().Result;
-                //text = await client.GetStringAsync("http://[2001:250:5800:11::1]/cgi-bin/rad_user_info"); //Ipv6认证地址未知
-            }
-            catch (Exception)
-            {
-                data.Add("校园网状态 : 请求失败");
-                return new Tuple<TestResult, string, IRepair>(TestResult.Failed,string.Join("\n",data), null);
-            }
+            var response = HttpUtil.GetHttpResponse(url);
+            response.EnsureSuccessStatusCode();
+            var text = response.Content.ReadAsStringAsync().Result;
 
             if (text.Contains("not_online_error"))
             {
-                data.Add("校园网状态 : 离线");
-                return new Tuple<TestResult, string, IRepair>(TestResult.Failed, string.Join("\n", data), null);
+                _isSuccess = false;
+                return "校园网状态 : 离线";
             }
+
+            var strBuilder = new StringBuilder();
             var infos = text.Split(',');
-            data.Add("校园网状态 : 在线");
-            data.Add($"校园网用户 : {infos[0]}");
-            data.Add($"校园网IP地址 : {infos[8]}");
-            return new Tuple<TestResult, string, IRepair>(TestResult.Success, string.Join("\n", data), null);
+
+            strBuilder.AppendLine("校园网状态 : 在线");
+            strBuilder.AppendLine($"校园网用户 : {infos[0]}");
+            strBuilder.AppendLine($"校园网IP地址 : {infos[8]}");
+
+            return strBuilder.ToString();
+        }
+        catch (Exception)
+        {
+            _isSuccess = false;
+            return "校园网状态 : 请求失败";
         }
     }
 }
